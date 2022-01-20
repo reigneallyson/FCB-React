0b78e4523e8f6adb3d84feea0a6787e7efa99ac5
Reigne Allyson De Leon <deleon.reigneallyson16@gmail.com>
2022-01-20 15:37:01 +0800

    Hello World Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// My first component
+function HelloWorld() {
+  return <h1>Hello, world!</h1>;
+}
+
+ReactDOM.render(<HelloWorld></HelloWorld>, document.getElementById("root"));
