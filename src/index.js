39067ca2d9bef5d635af6f07e78c3668f6146f21
Reigne Allyson De Leon <deleon.reigneallyson16@gmail.com>
2022-01-20 16:44:26 +0800

    Create a new TasksList Component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 function App() {
+
+function TasksList(){
+
+return(
+
+<ul>
+          <li className="tasks-item">Reigne
+          </li>
+ 
+          <li className="tasks-item"> Allyon </li>
+          <li className="tasks-item"> De Leon</li>
+        </ul>
+
+
+        );
+
+}
+const paraStyle = {
+
+  "margin-bottom": "15px",
+  "backgroundColor": "red"
+};
+
   return (
     <div className="app">
       <header className="app-header">
+
         <h1>My Amazing ToDo-List App</h1>
-        <p>The most simple and amazing todo-list React app.</p>
+        <p style = {paraStyle}>The most simple and amazing todo-list React app.</p>
+
+<TasksList />      
+
       </header>
     </div>
   );
